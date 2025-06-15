fdd4f876f45560155d10df1f4a6476abbc61d587
Dalin Stone <dalinkstone@gmail.com>
2025-06-14 19:27:50 -0600

    Exercise number 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+	    Random randomGenerator = new Random();
+	    int number = randomGenerator.Next(1, 100);
+		
+	    string response = "0";
+
+	    Console.WriteLine("A magic number has been selected. Guess the magic number to win the game!");
+
+	    while (int.Parse(response) != number)
+	    {
+		Console.Write("What is your guess? ");
+		response = Console.ReadLine();
+
+		if (int.Parse(response) > number)
+		{
+			Console.WriteLine("Lower");
+		}
+		else if (int.Parse(response) < number)
+		{
+			Console.WriteLine("Higher");
+		}
+		else 
+		{
+			Console.WriteLine("You guessed it!");
+		}
+	    }
+    }	
 }
