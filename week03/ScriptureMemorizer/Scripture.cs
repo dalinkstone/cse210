f2a5211d0f051357e161f9f856c87893365c3a3d
Dalin Stone <dalinkstone@gmail.com>
2025-06-26 22:05:19 -0600

    builds out the basic design of the program

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,16 @@
+public class Scripture
+{
+    private List<Word> _words;
+    private Reference _reference;
+
+    public Scripture(Reference Reference, string text) { }
+
+    public void HideRandomWords(int numberToHide) { }
+
+    public string GetDisplayText()
+    {
+        return;
+    }
+
+    public bool IsCompletelyHidden() { }
+}
