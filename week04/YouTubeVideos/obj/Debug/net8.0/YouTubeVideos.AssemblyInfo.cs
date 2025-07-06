826f779af7059895b26c4438a3aa210b19dc4f9c
Dalin Stone <dalinkstone@gmail.com>
2025-07-05 20:20:43 -0600

    this adds the execution example screenshot

--- week04/YouTubeVideos/obj/Debug/net8.0/YouTubeVideos.AssemblyInfo.cs
+++ week04/YouTubeVideos/obj/Debug/net8.0/YouTubeVideos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310723ffdb7fe80c74e4eb70f541eec9a3ee6bb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2109c6be99e7d4ba390b434b2599a0f8dcd1be")]
 [assembly: System.Reflection.AssemblyProductAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
