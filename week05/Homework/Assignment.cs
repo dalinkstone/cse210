42f6f9f38854a121eb4407a101268f5c636636ac
Dalin Stone <dalinkstone@gmail.com>
2025-07-09 17:31:01 -0600

    ran the linter

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -1,16 +1,16 @@
-public class Assignment 
+public class Assignment
 {
-	protected string _studentName;
-	private string _topic;
+    protected string _studentName;
+    private string _topic;
 
-	public Assignment(string studentName, string topic)
-	{
-		_studentName = studentName;
-		_topic = topic;
-	}
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
 
-	public string GetSummary()
-	{
-		return $"{_studentName} - {_topic}";
-	}
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
 }
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Assignment newAssignment = new Assignment("Samuel Bennet", "Multiplication");
-	Console.WriteLine(newAssignment.GetSummary());
-        
-	Console.WriteLine();
-        
-	MathAssignment newMathAssignment = new MathAssignment(
+        Console.WriteLine(newAssignment.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment newMathAssignment = new MathAssignment(
             "Roberto Rodriguez",
             "Fractions",
             "7.3",
@@ -17,11 +17,15 @@ class Program
         );
         Console.WriteLine(newMathAssignment.GetSummary());
         Console.WriteLine(newMathAssignment.GetHomeworkList());
-        
-	Console.WriteLine();
 
-	WritingAssignment newWritingAssignment = new WritingAssignment("Mandy Water", "European History", "The Causes of World War II");
-	Console.WriteLine(newWritingAssignment.GetSummary());
-	Console.WriteLine(newWritingAssignment.GetWritingInformation());
+        Console.WriteLine();
+
+        WritingAssignment newWritingAssignment = new WritingAssignment(
+            "Mandy Water",
+            "European History",
+            "The Causes of World War II"
+        );
+        Console.WriteLine(newWritingAssignment.GetSummary());
+        Console.WriteLine(newWritingAssignment.GetWritingInformation());
     }
 }
