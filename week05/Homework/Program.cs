341a991c1b2f9625445017feaaab25dd256d56ec
Dalin Stone <dalinkstone@gmail.com>
2025-07-09 17:29:53 -0600

    finished the learning assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,7 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-	Assignment newAssignment = new Assignment("Samuel Bennet", "Multiplication");
+        Assignment newAssignment = new Assignment("Samuel Bennet", "Multiplication");
 	Console.WriteLine(newAssignment.GetSummary());
+        
+	Console.WriteLine();
+        
+	MathAssignment newMathAssignment = new MathAssignment(
+            "Roberto Rodriguez",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+        Console.WriteLine(newMathAssignment.GetSummary());
+        Console.WriteLine(newMathAssignment.GetHomeworkList());
+        
+	Console.WriteLine();
+
+	WritingAssignment newWritingAssignment = new WritingAssignment("Mandy Water", "European History", "The Causes of World War II");
+	Console.WriteLine(newWritingAssignment.GetSummary());
+	Console.WriteLine(newWritingAssignment.GetWritingInformation());
     }
 }
--- week05/Homework/WritingAssignment.cs
+++ week05/Homework/WritingAssignment.cs
@@ -1,4 +1,14 @@
-public class WritingAssignment
+public class WritingAssignment : Assignment
 {
+	private string _title;
 
+	public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+	{
+		_title = title;
+	}
+
+	public string GetWritingInformation()
+	{
+		return $"{_title} by {_studentName}";
+	}
 }
