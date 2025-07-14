36004355deeaf32a083adad3f49e87e1cfc4a6e2
Dalin Stone <dalinkstone@gmail.com>
2025-07-14 17:44:08 -0600

    this is the learning assignment for the week using polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square newSquare = new Square("Red", 3);
+	shapes.Add(newSquare);
+
+        Rectangle newRectangle = new Rectangle("Blue", 3, 7);
+	shapes.Add(newRectangle);
+
+        Circle newCircle = new Circle("Yellow", 10);
+	shapes.Add(newCircle);
+
+	foreach (Shape shape in shapes)
+	{
+		string color = shape.GetColor();
+
+		double area = shape.GetArea();
+
+		Console.WriteLine($"The shapes color is {color} and the shapes area is {area}.\n");
+	}
     }
 }
